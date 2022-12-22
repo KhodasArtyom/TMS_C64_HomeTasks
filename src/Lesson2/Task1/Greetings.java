0db71aff7f6c7eac36303cdb6070a9ada1d05ce4
Artyom <a.Khodas1601@gmail.com>
2022-12-22 19:15:04 +0300

    Request name ang age

---
+++ src/Lesson2/Task1/Greetings.java
@@ -0,0 +1,20 @@
+package Lesson2.Task1;
+
+import java.util.Scanner;
+
+public class Greetings {
+    private short currentYear=2022;
+
+    public void enterNameAndAge() {
+        System.out.print("Hello,please enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.print("Ok, " +name +". " + "now enter the date of your birth: ");
+        short yearOfBirth = scanner.nextShort();
+        byte age = (byte) (currentYear - yearOfBirth);
+
+
+        System.out.println("Thanks," + name + ". "+ "You are " + age +" "+ "years old. Welcome to our course" );
+
+    }
+}
---
+++ src/Lesson2/Task1/Main.java
@@ -0,0 +1,8 @@
+package Lesson2.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Greetings greetings = new Greetings();
+        greetings.enterNameAndAge();
+    }
+}
